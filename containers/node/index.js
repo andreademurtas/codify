5508a2a04af4a5903e578fd27e38df1e04985436
Andrea De Murtas <demurtas.1883216@studenti.uniroma1.it>
2022-04-07 14:25:48 +0200

    Interazione basic tra l'app node e mongodb

--- containers/node/index.js
+++ containers/node/index.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const {MongoClient} = require('mongodb');
+
 const app = express();
+
+async function connect_and_list() {
+  const uri = "mongodb://root:example@mongodb:27017?authSource=admin";
+  const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+  try {
+    await client.connect();
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  }
+  finally {
+    await client.close();
+  }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+
 app.listen(3000, () => {
+  connect_and_list().catch(console.error);
   console.log('Example app listening on port 3000!');
 });
