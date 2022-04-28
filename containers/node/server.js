52ddc3815c1b5bb23ab79ef313f7d879402251c4
Andrea De Murtas <demurtas.1883216@studenti.uniroma1.it>
2022-04-28 16:44:05 +0200

    added TODO

--- containers/node/server.js
+++ containers/node/server.js
@@ -161,6 +161,10 @@ app.get('/challenges', (req, res) => {
   res.sendFile(path.join(__dirname, '/static/templates/problem/problem.html'))
 });
 
+app.get('/user', restrict, function(req,res){
+  res.send("<h1>hello</h1>");
+});
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
