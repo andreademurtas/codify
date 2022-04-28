3e041b12f2527d00883d45ff759da53e1c6903f2
thoms2901 <thomas.kirschner2901@gmail.com>
2022-04-28 15:56:07 +0200

    commit signin

---
+++ containers/node/static/templates/signup/index.js
@@ -0,0 +1,19 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    alert(profile.getName());
+    alert(profile.getEmail());
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $("#image").attr('src', profile.getImageUrl());
+    $(".data").css("display", "block");
+    $(".g-signin2").css("display", "none");
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        alert("You have been signed out successfully");
+        $(".data").css("display", "none");
+        $(".g-signin2").css("display", "block");
+    });
+}
