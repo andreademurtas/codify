b955cd8800f9edeceadac0f260737246a119eed2
Stefano Galizia <Stefano.Galizia3000@gmail.com>
2022-06-15 11:37:06 +0200

    comments

--- containers/node/server.js
+++ containers/node/server.js
@@ -182,47 +182,33 @@ app.post('/signup', (req, res) => {
 	amqplib.connect('amqp://guest:guest@rabbitmq', (err, connection) => {
     		if (err) {
         		console.error(err.stack);
-      			//return process.exit(1);
     		}
 
-    		// Create channel
     		connection.createChannel((err, channel) => {
         		if (err) {
             			console.error(err.stack);
-            			//return process.exit(1);
         		}
 
-			//create queue
 			var queue = 'queue';
 	
-      			// Ensure queue for messages
       			channel.assertQueue(queue, {
-            		// Ensure that the queue is not deleted when server restarts
             		durable: true
         		}, err => {
             		if (err) {
               			console.error(err.stack);
-              			//return process.exit(1);
       				}
 
-            		// Create a function to send objects to the queue
-            		// Javascript object is converted to JSON and then into a Buffer
             		let sender = (content) => {
                 		let sent = channel.sendToQueue(queue, Buffer.from(JSON.stringify(content)), {
-                    		// Store queued elements on disk
                     		persistent: true,
                     		contentType: 'application/json'
                 		});
             		};
 
-            		// push message to queue
             		let sent = 0;
             		let sendNext = () => {
                	 	if (sent >= 1) {
                     			console.log('All messages sent!');
-                    			// Close connection to AMQP server
-                    			// We need to call channel.close first, otherwise pending
-                    			// messages are not written to the queue
                     			return channel.close(() => connection.close());
                 		}
                 		sent++;
--- containers/nodemailer/server.js
+++ containers/nodemailer/server.js
@@ -4,7 +4,6 @@
 const amqplib = require('amqplib/callback_api');
 const nodemailer = require('nodemailer');
 
-// Setup Nodemailer transport
 const transport = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
@@ -15,22 +14,18 @@ const transport = nodemailer.createTransport({
 
 transport.verify((error, success) => {
 	if (error) {
-    		//if error happened code ends here
     		console.error(error);
     	} 
     	else {
-    		//this means success
     		console.log('Ready to send mail!');
 	}
 })
 
-// Create connection to AMQP server
 amqplib.connect('amqp://guest:guest@rabbitmq', (err, connection) => {
     if (err) {
         console.error(err.stack);
         return process.exit(1);
     }
-    // Create channel
     connection.createChannel((err, channel) => {
         if (err) {
             console.error(err.stack);
@@ -39,9 +34,7 @@ amqplib.connect('amqp://guest:guest@rabbitmq', (err, connection) => {
         
         var queue = 'queue';
 
-        // Ensure queue for messages
         channel.assertQueue(queue, {
-            // Ensure that the queue is not deleted when server restarts
             durable: true
         }, err => {
             if (err) {
@@ -49,26 +42,17 @@ amqplib.connect('amqp://guest:guest@rabbitmq', (err, connection) => {
                 return process.exit(1);
             }
 
-            // Only request 1 unacked message from queue
-            // This value indicates how many messages we want to process in parallel
             channel.prefetch(1);
 
-            // Set up callback to handle messages received from the queue
             channel.consume(queue, data => {
                 if (data === null) {
                     return;
                 }
 
-                // Decode message contents
                 let message = JSON.parse(data.content.toString());
-                console.log("ciao" + message);
                 
-                //prendo email e password
                 let email = message.email;
                 let username = message.username;
-                
-                console.log(message.email);
-                console.log(message.username);
                 
                 var mailOptions = {
                 	from: 'codify045@gmail.com',
@@ -76,15 +60,11 @@ amqplib.connect('amqp://guest:guest@rabbitmq', (err, connection) => {
                 	text: 'Congratulazioni ' + username + ' ti sei registrato!'
                 }
 
-                // Send the message using the previously set up Nodemailer transport
                 transport.sendMail(mailOptions, (err, info) => {
                     if (err) {
                         console.error(err.stack);
-                        // put the failed message item back to queue
                         return channel.nack(data);
                     }
-                    console.log('Delivered message %s', info.messageId);
-                    // remove message item from the queue
                     channel.ack(data);
                 }); 
             }, { noAck: false });
